74412b17529586280fdf545527271f12181b03a7
s1280226 <63778886+Takahashi-Haruya@users.noreply.github.com>
2021-07-28 19:27:24 +0900

    Create sccp2021_todo.js

---
+++ sccp2021_todo.js
@@ -0,0 +1,25 @@
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const list = document.getElementById("list");
+let num = 0;
+
+function getTask(){
+	const task = input.value;
+  if(task == "" || num >= 10) return;
+  num++;
+  const listitem = document.createElement("li");
+  const reButton = document.createElement("button");
+  
+  listitem.innerText = task;
+  list.appendChild(listitem);
+  
+  reButton.innerText = "消去";
+  reButton.onclick = function(){
+  	const task2re = reButton.closest("li");
+    list.removeChild(task2re);
+    num--
+  }
+  listitem.append(reButton);
+}
+
+button.addEventListener("click", getTask);
